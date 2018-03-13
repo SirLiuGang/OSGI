489d8d6673fe327bfe092965b0c866cc399e775c
SirLiuGang <1627035841@qq.com>
2018-03-13 23:37:56 +0800

    尝试使用OSGI框架

---
+++ osgi_world/src/osgi_world/Activator.java
@@ -0,0 +1,24 @@
+package osgi_world;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hello LiuGang!!");
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye LiuGang!!");
+	}
+
+}
